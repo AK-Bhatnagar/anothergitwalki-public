b5f1d3e6ac0033020174e721bf330db6cc896104
AK-Bhatnagar <94451497+AK-Bhatnagar@users.noreply.github.com>
2023-01-17 17:55:55 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -4,6 +4,6 @@ print(os.environ['CNVRG_JOB_ID'])
 os.chdir('output')
 os.mkdir(path)
 for i in range (1, 10000):
-    f = open("{}/demofile{}.txt".format(path, i), "a")
+    f = open("{}/output/demofile{}.txt".format(path, i), "a")
     f.write("Now the file has more content!")
     f.close()
