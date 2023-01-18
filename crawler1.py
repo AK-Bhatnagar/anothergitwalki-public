402e3d6991c368c353787b6b36388ae39553d68d
AK-Bhatnagar <94451497+AK-Bhatnagar@users.noreply.github.com>
2023-01-18 12:06:18 +0530

    Update crawler1.py

--- crawler1.py
+++ crawler1.py
@@ -1,9 +1,10 @@
 import os
+
 path = "my-treerd-folder"
-print(os.environ['CNVRG_JOB_ID'])
-os.chdir('output')
-#os.mkdir(path)
-for i in range (1, 10000):
-    f = open("{}/demofile{}.txt".format(path, i), "a")
-    f.write("secondcontent")
+print(os.environ["CNVRG_JOB_ID"])
+os.chdir("output")
+# os.mkdir(path)
+for i in range(1, 10500):
+    with open("/exp_file_no_{}".format(i), "w") as f:
+        f.write("secondcontent")
     f.close()
