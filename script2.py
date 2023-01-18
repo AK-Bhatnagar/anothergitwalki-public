249c6649ddedafed406dab4de521127d85019387
AK-Bhatnagar <94451497+AK-Bhatnagar@users.noreply.github.com>
2023-01-18 11:29:08 +0530

    Update script2.py

--- script2.py
+++ script2.py
@@ -2,9 +2,8 @@ import os
 
 path = "my-treerd-folder"
 print(os.environ["CNVRG_JOB_ID"])
-os.chdir("output")
 os.mkdir(path)
-for i in range(10500):
+for i in range(1, 10000):
     with open("output/exp_file_no_{}".format(i), "w") as f:
-        f.write(str(i))
+        f.write("Now the file has more content!")
     f.close()
