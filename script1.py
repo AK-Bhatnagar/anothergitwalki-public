7ccfc3f08e752a7a74dac91e2831d1324e3ccaf3
AK-Bhatnagar <94451497+AK-Bhatnagar@users.noreply.github.com>
2023-03-29 16:58:25 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -6,4 +6,5 @@ os.mkdir(path)
 for i in range (1, 10000):
     f = open("{}/demofile{}.txt".format(path, i), "a")
     f.write("Now the file has more content!")
+    f.write("Now the file has more content!")
     f.close()
