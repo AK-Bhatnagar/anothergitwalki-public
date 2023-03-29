7184b4d9097dbaa8fc92aafe1379618f78c76a5a
AK-Bhatnagar <94451497+AK-Bhatnagar@users.noreply.github.com>
2023-03-29 17:08:26 +0530

    Update script2.py

--- script2.py
+++ script2.py
@@ -7,3 +7,7 @@ for i in range(1, 10000):
     with open("output/exp_file_no_{}".format(i), "w") as f:
         f.write("Now the file has more content!")
     f.close()
+for i in range(1, 10000):
+    with open("output/exp_file_no_{}".format(i), "w") as f:
+        f.write("Now the file has more content!")
+    f.close()
