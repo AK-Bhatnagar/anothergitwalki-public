300eb91b81712087d142aacce78f547f2c36bd59
AK-Bhatnagar <94451497+AK-Bhatnagar@users.noreply.github.com>
2023-03-29 16:41:32 +0530

    Update sleep.py

--- sleep.py
+++ sleep.py
@@ -1,6 +1,2 @@
 print(" I am on another public repo...new commit")
-print(" I am on another public repo...new commit")
-print(" I am on another public repo...new commit")
-print(" I am on another public repo...new commit")
-print(" I am on another public repo...new commit")
-print(" I am on another public repo...new commit")
+
