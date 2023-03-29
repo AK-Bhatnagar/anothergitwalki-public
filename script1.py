ea4e679a903c493720378eaac0cd2035e3514ce4
AK-Bhatnagar <94451497+AK-Bhatnagar@users.noreply.github.com>
2023-03-29 17:08:08 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -8,3 +8,8 @@ for i in range (1, 10000):
     f.write("Now the file has more content!")
     f.write("Now the file has more content!")
     f.close()
+for i in range (1, 10000):
+    f = open("{}/demofile{}.txt".format(path, i), "a")
+    f.write("Now the file has more content!")
+    f.write("Now the file has more content!")
+    f.close()
